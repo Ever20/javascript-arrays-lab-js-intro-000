9beebf5c94cbb832b03ae932cf646a667bfd0a42
Francisco trejo <f85.trejo@gmail.com>
2021-04-27 18:41:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var kittens = [] //define your array here
+var kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];
+
+function destructivelyAppendKitten(myArray, name) {
+  myArray.push(name);
+  return 'Ralph'
+};
+//define your array here
 
 // Add your functions and code here
